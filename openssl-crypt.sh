61d9b495b7ae142bc0d3daa289995dbbb8fcbaa3
yAzZiE Labs <unclenerdicus@yazzietech.com>
2013-12-07 16:38:17 -0500

    Update openssl-crypt.sh

--- openssl-crypt.sh
+++ openssl-crypt.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 # Copyright 2013 yAzZiE Labs
 #
@@ -13,7 +13,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 function home {
